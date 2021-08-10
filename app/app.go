8d2e6b61df85b225910b6ca35d18a2887f3e2120
Pogula Pavani <pogulapavani@gmail.com>
2021-08-10 14:44:03 +0530

    refactoring the code

--- app/app.go
+++ app/app.go
@@ -2,20 +2,10 @@ package app
 
 import (
 	"fmt"
-	"net/http"
 )
 
 func StartServer() {
-	if err := http.ListenAndServe(":8080", pingHandler()); err != nil && err != http.ErrServerClosed {
-		fmt.Errorf("something went wrong %s", err)
-		fmt.Println("Server Not Started")
-		return
-	}
-	fmt.Println("Server Started")
-}
-
-func pingHandler() http.HandlerFunc {
-	return func(writer http.ResponseWriter, request *http.Request) {
-		writer.WriteHeader(http.StatusOK)
-	}
+	r := CreateRouter()
+	fmt.Println("Starting the Server")
+	r.Run(":8080")
 }
